91812909f61a838b1892596cdf1858d09629276f
MaheshGanji <msganji9998@gmail.com>
2024-08-29 08:19:21 +0530

    Update provider.tf
    region updated

--- provider.tf
+++ provider.tf
@@ -10,5 +10,5 @@ terraform {
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region     = var.aws_region
+  region     =  "ap-south-1"  # var.aws_region
 }
